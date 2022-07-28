5adc4bb4d061dcca8f4871b79a11e1364aeb27b6
Juan Ignacio Saccardo <juan.saccardo@hotmail.com>
2022-07-27 22:29:44 -0300

    Segundo Commit 27/07/22

--- Guia 7 - Programacion Orientada a Objetos/javaGuia7_Extra6/src/Juego/Entidades/Ahorcado.java
+++ Guia 7 - Programacion Orientada a Objetos/javaGuia7_Extra6/src/Juego/Entidades/Ahorcado.java
@@ -21,7 +21,10 @@ public class Ahorcado
     private String palabra[];
     private int aciertos;
     private int intentos;
+    
+    //Este lo voy a usar para ir imprimiendo la palabra a medida que la va adivinando
     private String aux2[];
+    
     Scanner leer = new Scanner(System.in).useDelimiter("\n");
 
     public Ahorcado(String[] palabra, int aciertos, int intentos) {
@@ -49,6 +52,20 @@ public class Ahorcado
     public void setintentos(int intentos) {
         this.intentos = intentos;
     }
+
+   public void imprimir() 
+   {    
+       for (int i=0;i<palabra.length;i++)
+       {
+            System.out.print(palabra[i]);
+       }
+       for (int i=0;i<palabra.length;i++)
+       {
+            System.out.print(aux2[i]);
+       }
+       
+        System.out.println("Ahorcado{" + "palabra=" + palabra + ", aciertos=" + aciertos + ", intentos=" + intentos + ", aux2=" + aux2 + '}');
+   }
     
     
     /**
@@ -65,6 +82,9 @@ valor que ingresó el usuario y encontradas en 0.
         
         //Asigno el largo al vector segun el largo de "pal"
         palabra = new String[pal.length()];
+        
+        //Le asigno el mismo largo al vector aux2, y lo lleno con guiones bajos. 
+        //Cuando el usuario adivine una letra, reemplazo los guiones con la letra
         aux2 = new String[pal.length()];
         Arrays.fill(aux2,"_");
         
@@ -102,7 +122,7 @@ letra ingresada es parte de la palabra o no. También informará si la letra est
         String aux[]=Arrays.copyOf(palabra, palabra.length);
         Arrays.sort(aux);
         int pivot=Arrays.binarySearch(aux,busqueda);
-        
+                
         if(pivot>=0)
             System.out.println("La letra esta contenida en la palabra");
         else{
@@ -127,10 +147,14 @@ busque una letra que no esté, se le restará uno a sus oportunidades.*/
             }
         }
         System.out.println("Número de letras (encontradas, faltantes): ("+aciertos+","+((palabra.length)-aciertos)+")");
+        
+        // Aca voy imprimiendo aux2, con las letras adivinadas dentro de la palabra
         for (int i=0;i<palabra.length;i++) 
         {
             System.out.print(" "+aux2[i]+" ");
         }
+        
+        //Con esto busco si la letra ingresada esta en la palabra y devuelvo true si está
         String aux[]=Arrays.copyOf(palabra, palabra.length);
         Arrays.sort(aux);
         int pivot=Arrays.binarySearch(aux,busqueda);
@@ -138,12 +162,26 @@ busque una letra que no esté, se le restará uno a sus oportunidades.*/
         check = true;
         return check;
     }
+    
     //Método intentos(): para mostrar cuantas oportunidades le queda al jugador.
     public void intentos()
     {   
         System.out.println("Intentos restantes: "+intentos);
-        
     }
+    
+    //Con este metodo reviso si la letra que el usuario ingresó ya la tengo como 
+    //"revelada" en el vector aux2, y si esta, devuelvo true.    
+    public boolean letraRepetida(String busqueda)
+    {   
+        boolean repetida=false;
+        for (int i=0;i<palabra.length;i++)
+        {  
+            if(aux2[i].equals(busqueda))
+                repetida=true;
+        }    
+        return repetida;
+    }
+    
     
     /*Método juego(): el método juego se encargará de llamar todos los métodos
 previamente mencionados e informará cuando el usuario descubra toda la palabra o
@@ -157,12 +195,21 @@ se quede sin intentos. Este método se llamará en el main.*/
         do{
         System.out.println("Ingrese una letra para buscar en la palabra");
         String busqueda = leer.next();
-        
-        // Incluir un metodo para verificar si la letra ya habia sido elegida anteriormente
-        
+                
+        // Aca verifico si ya adivine esa letra, a traves del metodo letraRepetida.
+        // Si ya lo habia adivinado, le pido otra.
+        while (letraRepetida(busqueda))
+        {
+            System.out.println("Ya ha adivinado esa letra. Elija otra: ");
+            busqueda=leer.next();
+        }
+                
         buscarLetra(busqueda);
         encontrarLetra(busqueda);
         System.out.println("Oportunidades restantes:"+intentos);
+        
+        //Aca comparo cantidad de aciertos con el largo de la palabra.
+        //Si son iguales, significa que ya adivine todas las letras
         if(aciertos==palabra.length)
         {
             System.out.println("Has adivinado la palabra!!");
@@ -171,4 +218,5 @@ se quede sin intentos. Este método se llamará en el main.*/
         }while(aciertos<palabra.length&&intentos>0);
     }
     
+   
 }
