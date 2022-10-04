6fc2255bd651765a2e5a30016441e0b5563f69f5
Juan Ignacio Saccardo <juan.saccardo@hotmail.com>
2022-10-03 21:58:33 -0300

    Commit 2 03/10/22

--- Guia 11 - Excepciones/javaGuia11_Ej3/src/Main.java
+++ Guia 11 - Excepciones/javaGuia11_Ej3/src/Main.java
@@ -1,20 +1,60 @@
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+
+Defina una clase llamada DivisionNumero. En el método main utilice un Scanner para leer dos
+números en forma de cadena. A continuación, utilice el método parseInt() de la clase Integer,
+para convertir las cadenas al tipo int y guardarlas en dos variables de tipo int. Por ultimo realizar
+una división con los dos numeros y mostrar el resultado. 
+
+Todas estas operaciones puede tirar excepciones a manejar, el ingreso por teclado puede
+causar una excepción de tipo InputMismatchException, el método Integer.parseInt() si la cadena
+no puede convertirse a entero, arroja una NumberFormatException y además, al dividir un
+número por cero surge una ArithmeticException. Manipule todas las posibles excepciones
+utilizando bloques try/catch para las distintas excepciones
+
  */
 
-/**
- *
- * @author juans
- */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) 
+    {
+        Scanner leer = new Scanner(System.in).useDelimiter("\n");
+        String a;
+        String b;
+        try
+        {
+            System.out.println("Ingrese el primer numero (Numerador)");
+            a=leer.next();
+            System.out.println("Ingrese el segundo numero (Denominador)");
+            b=leer.next();
+            int c = Integer.parseInt(a);
+            int d = Integer.parseInt(b);
+            DividirNumeros(c, d);
+            
+        }
+        catch (InputMismatchException e)
+        {
+              System.out.println("El valor ingresado no es un entero");      
+        }
+        catch(ArithmeticException f)
+        {
+            System.out.println("No se puede dividir por cero");
+        }
+        catch(NumberFormatException h)
+        {
+            System.out.println("El valor ingresado no es un entero");
+        }
+        
+        
     }
+    
+    public static void DividirNumeros(int a, int b)
+    {
+        System.out.println("Division: "+a/b);
+    }
+           
     
 }
